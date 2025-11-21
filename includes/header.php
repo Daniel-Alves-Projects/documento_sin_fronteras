0cbe59709c248996d7012d6568c3852e6ea99666
Daniel-Alves-Projects <contato@danielalves.tec.br>
2025-11-20 23:16:15 -0300

    inicial commit

---
+++ includes/header.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="document_sin_fronteiras">
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:title" content="NestSafe | ">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="">
+
+    <link rel="icon" href="" type="image/png">
+    <link rel="stylesheet" href="style.css">
+
+    <script src="script.js"></script>
+
+
+    <title>Inicio</title>
+</head>
+
+<header>
+    <nav class="navbar">
+        <ul class="nav-link">
+            <li class="nav-item">
+                <a href="#home">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a href="#servicos">Serviços</a>
+            </li>
+            <li class="nav-item">
+                <a href="#sobre">Sobre</a>
+            </li>
+            <li class="nav-item">
+                <a href="#contato">Contato</a>
+            </li>
+            <li class="nav-item">
+                <a href="#blog">Blog</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+</html>
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include('include/header.php')?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include('include/footer.php') ?>
