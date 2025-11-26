99c134b96e8d2210eeff84a756402ef84261ad85
Daniel-Alves <assistenciatecnica@ballke.com.br>
2025-11-26 13:17:17 -0300

    criados e estilizados cards de serviços

--- index.php
+++ index.php
@@ -32,7 +32,87 @@ include('includes/config.php');?>
         <span class="visually-hidden">Próximo</span>
     </button>
 </div>
+<div class="services-section">
+    <div class="services-content">
+        <a href="" class="services-cards refugio">
+            <div class="cards-info">
+                <h1>Desarchivamiento del proceso de refugio</h1>
+                <p>"Si tu proceso de refugio ha sido archivado, nosotros nos encargamos de su reactivación total. Gestionamos el complejo Pedido de Desarchivamiento a través de la plataforma SEI, garantizando que tu expediente se reabra sin complicaciones. De esta manera, podrás volver a renovar tu documentación migratoria y asegurar la continuidad de tu estatus en Brasil."</p>
+            </div>
+        </a>
 
+        <a href="" class="services-cards gestao">
+            <div class="cards-info">
+                <h1>Gestión completa de la Solicitud de Refugio</h1>
+                <p>"Aceleramos tu proceso de refugio en Brasil. Nuestro equipo asegura la carga exitosa de tu información en el Sisconare y gestiona la programación de tu cita en la Policía Federal, garantizándote la pronta emisión de tu Protocolo de Solicitud."</p>
+            </div>
+        </a>
+
+        <a href="" class="services-cards protocolo">
+            <div class="cards-info">
+                <h1>Renovar Protocolo de Refugio</h1>
+                <p>"Te brindamos tranquilidad legal en Brasil. Nos encargamos proactivamente de la renovación anual de tu Protocolo de Refugio y tu RNM, eliminando el riesgo de caer en la irregularidad y asegurando la continuidad de todos tus derechos en el país."</p>
+            </div>
+        </a>
+
+        <a href="" class="services-cards policia-federal">
+            <div class="cards-info">
+                <h1>Agendamiento para la Policía Federal</h1>
+                <p>"Garantizamos tu lugar en la agenda de la Policía Federal. Nos ocupamos de programar tus citas estratégicamente para que no pierdas turnos ni disponibilidad, lo que resulta fundamental para evitar demoras en la obtención de tus documentos."</p>
+            </div>
+        </a>
+
+        <a href="" class="services-cards direccion">
+            <div class="cards-info">
+                <h1>Cambio de Endereço(Dirección)</h1>
+                <p>"Evita problemas de entrega. Realizamos la actualización obligatoria de tu domicilio en los registros del SISMIGRA, evitando que tu RNM plástico se envíe a una dirección incorrecta y garantizando que esté disponible para ser recogido en la Policía Federal más cercana a ti."</p>
+            </div>
+        </a>
+
+        <a href="" class="services-cards sisconare">
+            <div class="cards-info">
+                <h1>Respuesta en el Sisconare(Diligencia)</h1>
+                <p>"Cumplimos a tiempo con los requerimientos del CONARE. Nos encargamos de formalizar y subir tu respuesta en el Sisconare antes de la fecha límite. Esta gestión precisa es vital para mantener tu proceso activo y evitar el archivamiento por incumplimiento."</p>
+            </div>
+        </a>
+
+        <a href="" class="services-cards traduccion">
+            <div class="cards-info">
+                <h1>Traducción Juramentada</h1>
+                <p>"Validez Legal Garantizada para tus Documentos. Nos encargamos de que tus documentos del país de origen sean plenamente válidos en Brasil. Nuestro equipo coordina la traducción juramentada exclusivamente con traductores públicos acreditados, incluyendo la autenticación de la firma en cartório, lo que asegura su aceptación en cualquier institución oficial o privada."</p>
+            </div>
+        </a>
+
+        <a href="" class="services-cards curriculum">
+            <div class="cards-info">
+                <h1>Currículum Professional : La Llave a tu Próximo Empleo</h1>
+                <p>"Optimización de Currículum para el Trabajo Brasileño. Diseñamos tu currículum profesional en portugués, adaptándolo completamente a las normas, formatos y expectativas del entorno laboral de Brasil. Esto incrementa significativamente tus oportunidades de ser seleccionado para entrevistas y conseguir un empleo de calidad."</p>
+            </div>
+        </a>
+
+        <a href="" class="services-cards cadastro-sei">
+            <div class="cards-info">
+                <h1>Cadastro en el SEI</h1>
+                <p>El Sistema Electrónico de Información (SEI) es una herramienta de gestión de documentos y procesos electrónicos utilizada por diversos órganos y entidades del sector público de Brasil (federal, estatal y municipal).</p>
+            </div>
+        </a>
+
+        <a href="" class="services-cards cpf">
+            <div class="cards-info">
+                <h1>Siolicitud de CPF</h1>
+                <p>"Tramitación Rápida del CPF ante la Receita Federal. Gestionamos todos los requerimientos relacionados con tu Cadastro de Pessoa Física: solicitud, actualización de información y emisión de una nueva tarjeta. Obtén o regulariza este documento clave para poder realizar cualquier operación legal en Brasil."</p>
+            </div>
+        </a>
+
+        <a href="" class="services-cards visa-familiar">
+            <div class="cards-info">
+                <h1>Visa para reunión familiar ok</h1>
+                <p>“Tu vida en Brasil empieza hoy. Olvídate de la burocracia. Garantizamos tu Residencia Permanente o Visa Familiar, ya sea por matrimonio, vínculo con tu hijo brasileño o reunión familiar. Nosotros nos encargamos de cada paso.”</p>
+            </div>
+        </a>
+    </div>
+
+</div>
 
 
     
